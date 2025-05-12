e89532d96fb6f9613ff3a9b6b3965077ce3b0665
Mulyadi <mulyadig165@gmail.com>
2025-05-12 17:39:34 +0700

    Customer Done

--- app/Services/Notification/Channels/WAChannel.php
+++ app/Services/Notification/Channels/WAChannel.php
@@ -6,8 +6,7 @@ class WAChannel implements NotificationChannelInterface
 {
     public function send(array $data): string
     {
-        // Simulasi pengiriman WA
-        \Log::info("WA sent to {$data['to']}: {$data['message']}");
-        return "WA sent to {$data['to']}";
+        // Mock: return log / print pesan WA
+        return "WA sent to {$data['to']}: {$data['message']}";
     }
 }
