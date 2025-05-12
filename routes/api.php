60b91045171a55b1c0ac07a44d1f862b80361855
Mulyadi <mulyadig165@gmail.com>
2025-05-12 20:37:42 +0700

    Notifikasi Done

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\NotificationController;
 
 Route::post('/notify', [NotificationController::class, 'send']);
 Route::post('/register', [CustomerController::class, 'register']);
 Route::post('/login', [CustomerController::class, 'login']);
+
+
